4de8146694e751d5f009645b3b6e7cf826fbdbfb
baosws <baosws@gmail.com>
2022-11-22 19:35:41 +1100

    remove redundant files

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,5 @@
 import contextlib, time
-from socket import gethostname
 from typing import NamedTuple
-from itertools import chain, combinations
 import numpy as np
 import yaml
 
@@ -19,31 +17,6 @@ def Timer(name=None, verbose=False):
     if verbose:
         print(f'{name:<6}: {timer.elapsed:.3f}s')
 
-def CIT_wrapper(cit, **kwargs):
-    def func(dm, i, j, k):
-        k = list(k)
-        X = dm[:, i]
-        Y = dm[:, j]
-        Z = dm[:, k]
-        return cit(X, Y, Z, **kwargs)
-    setattr(func, '__name__', cit.__name__)
-    return func
-
-def power_set(d):
-    return chain.from_iterable(combinations(range(d), k) for k in range(1, d + 1))
-
-def is_on_server():
-    servers = [
-        'login',
-        'cpu-01',
-        'cpu-02',
-        'cpu-03',
-        'gamling-4',
-        'gamling-5',
-        'gamling-6',
-        'gamling-7',
-    ]
-    return gethostname() in servers
 
 def strip_outliers(x):
     l, r = np.quantile(x, q=[0.025, 0.975], axis=0)
